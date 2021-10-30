8e75c78e4eab35bc35be45b870a2eb36fb29d681
solitude <solitude@protogalaxy.me>
2021-10-31 00:10:23 +0900

    update

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -5,14 +5,14 @@ database_service_dir="database-service"
 secure_dir="vault"
 service_dir="service"
 
-mkdir -p /etc/$core_dir/proxy/server_blocks
-mkdir -p /etc/$core_dir/$database_service_dir/mysql/data
-mkdir -p /etc/$core_dir/$database_service_dir/mysql/config
-mkdir -p /etc/$core_dir/$database_service_dir/redis/data
-mkdir -p /etc/$core_dir/$secure_dir/logs
-mkdir -p /etc/$core_dir/$secure_dir/file
-mkdir -p /etc/$core_dir/$service_dir/wordpress/core
-mkdir -p /etc/$core_dir/$service_dir/owncloud/apps
-mkdir -p /etc/$core_dir/$service_dir/owncloud/config
-mkdir -p /etc/$core_dir/$service_dir/owncloud/data
-mkdir -p /etc/$core_dir/$service_dir/jenkins/data
+sudo mkdir -p /etc/$core_dir/proxy/server_blocks
+sudo mkdir -p /etc/$core_dir/$database_service_dir/mysql/data
+sudo mkdir -p /etc/$core_dir/$database_service_dir/mysql/config
+sudo mkdir -p /etc/$core_dir/$database_service_dir/redis/data
+sudo mkdir -p /etc/$core_dir/$secure_dir/logs
+sudo mkdir -p /etc/$core_dir/$secure_dir/file
+sudo mkdir -p /etc/$core_dir/$service_dir/wordpress/core
+sudo mkdir -p /etc/$core_dir/$service_dir/owncloud/apps
+sudo mkdir -p /etc/$core_dir/$service_dir/owncloud/config
+sudo mkdir -p /etc/$core_dir/$service_dir/owncloud/data
+sudo mkdir -p /etc/$core_dir/$service_dir/jenkins/data
